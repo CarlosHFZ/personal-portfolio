4a4f5ac9d622c88ef97795344674832ffbb4c1c1
CarlosHFZ <kalizehnder@outlook.com>
2025-04-23 20:14:58 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,8 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
-// exporta uma função async
 export default async () => {
-  const plugins = [
-    react(),
-    runtimeErrorOverlay(),
-  ];
+  const plugins = [react(), runtimeErrorOverlay()];
 
   if (process.env.NODE_ENV !== "production" && process.env.REPL_ID !== undefined) {
     const { cartographer } = await import("@replit/vite-plugin-cartographer");
